0a8ad0ddfea26c100e27fa151c94d0fbd7150aa7
Mushfiqur-Rahman-Apon <rahmanapon600@gmail.com>
2026-08-13 03:28:49 +0600

    Add files via upload

---
+++ Basic Data structure/Same_or_Not.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+        int val;
+        Node* next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+pr
+
+
+int main()
+{   
+    stack<int> st;
+    int n;
+    cin >> n;
+    for(int i=0;i<n;i++)
+    {
+        int [n];
+        cin >> v;
+        st.push(v);
+    }
+    printStack(st);
+
+    return 0;
+}
